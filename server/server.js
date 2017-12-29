1fece378c1ddd474eea5abfe54beeccf38be49d2
harshit <hrt1993@gmail.com>
2017-12-29 19:20:01 +0530

    Add/Get todos route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ var app=express();
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=> {
+   
 var todo = new Todo({
     text: req.body.text
 });
@@ -24,6 +25,19 @@ todo.save().then((doc)=>{
 });
 });
 
+
+app.get('/todos',(req, res)=>{
+    Todo.find().then((todos)=>{
+res.send({todos});
+    },(e)=>{
+res.status(400).send(e);
+    } )
+});
+
+
 app.listen(3000,() =>{
     console.log('Started on port 3000');
 });
+
+
+module.exports={app};
